970539c463a653b83512bec569e6c0185d2602d8
MwirigiBrianBob21 <brian.mwirigi@students.moringaschool.com>
2022-09-20 18:59:51 +0300

    Complete assignment

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,7 +1,41 @@
 import React from "react";
 
+
+// Child component of BlogPost().
+// Only way to pass information is through props
 function BlogContent(props) {
-  return <div id="blog-content">{props.articleText}</div>;
+  if (!props.isPublished) {
+    return null;
+    // return null means no DOM elements should be displayed here
+    // so, unpublished content- isPublished is not true, do not publish
+
+  } else {
+    // show published content
+    return (
+      <div>
+        <h1>{props.articleText}</h1>
+        <p>{props.minutesToRead} minutes to read</p>
+      </div>
+    );
+  }
 }
 
 export default BlogContent;
+
+
+
+// .....................personal work.....................
+
+// // function ParentComponent(props)
+// function BlogContent(props) {
+//   return <div>{props.articleText}</div>
+// }
+
+// // function ChildComponent
+// function BlogPost() {
+//   return (
+//     <div>
+//       <BlogContent articleText = "Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
+//     </div>
+//   );
+// }
--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -2,10 +2,16 @@ import React from "react";
 import BlogContent from "./BlogContent";
 import Comment from "./Comment";
 
+// Parent Component to Blogcontent(props)- props are many, comment(props)-prop is commentText that 
+// borrows information from this component.
 function BlogPost() {
   return (
     <div id="blog-post">
-      <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
+      <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." isPublished = {true}
+      minutesToRead = {1}/>
+      {/* for props that are strings, we do not need curly braces, however we need them for other 
+      data types like boolean, numbers, objects, arrays, etc */}
+      
       <Comment commentText="I agree with this statement. - Angela Merkel" />
       <Comment commentText="A universal truth. - Noam Chomsky" />
       <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
